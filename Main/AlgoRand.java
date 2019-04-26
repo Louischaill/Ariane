05908af26204ac5cea0fd15808b10f7700dc32a4
Louischaill <louischaillou2@gmail.com>
2019-04-26 22:42:46 +0200

    algorand

--- Main/AlgoRand.java
+++ Main/AlgoRand.java
@@ -10,84 +10,110 @@ import java.lang.Math;
     	private GrilleCrea e;
     	private int res=0;
 
-		public AlgoRand(GrilleCrea e,int[] tab , int sauvx, int sauvy,int taille){
-			System.out.println("Finis");
+		public AlgoRand(GrilleCrea e,int[] tab , int sauvx, int sauvy, int taille){
+		/*	System.out.println("Finis");*/
 			this.e=e;
 			Random random = new Random();
-			int nb;
+			int nb,i;
+			int fin=0;
+			int compteur=0;
 
-		for(int i =0 ; res!= 1 ; i++){
-     	
+		for( i = 0 ; fin != 8; i++){
 			nb = random.nextInt(4);
 
-	     	
-     	if(nb==0){
+			if(nb==0){
+				System.out.println("haut");
+     	 			
+	     		if((sauvx%taille) != 0 ){
+	     			if(tab[sauvx-1]==3){
+		   				System.out.println("FInis");
+		   				tab[sauvx]=0;
+	     				fin=8;
+		     		}
+	     			if(tab[sauvx-1]==0 ){
+		  				System.out.println("possible haut");
+	    				int res = sauvx-1;
+	    				tab[res]=tab[sauvx];
+		     			tab[sauvx]=0;
+		     			sauvx=res;
+		     			compteur++;
+					}	     				
+	     		}
+	     	}
+			
 
-     		if(tab[sauvx-1]== 3){
-     			tab[sauvx-1]=2;
-     			System.out.println("Finis !");
-     			res=1;
-     		}else{     	
-	     		if((sauvx%taille) != 0 && tab[sauvx-1]!=1 ){
-	     			tab[sauvx-1]=2;
-	     			tab[sauvx]=0;
-	     			e.repaint();
-	     			/*System.out.println("Haut");*/
+			if(nb==1){
+				System.out.println("bas");
+     	 		
+	     			if((sauvx%taille) != (taille-1)){
+	     				System.out.println("test");
+	     				if(tab[sauvx+1]==3){
+		     				System.out.println("FInis");
+		     				tab[sauvx]=0;
+		     				fin=8;
+		     			}
+	     				if(tab[sauvx+1]==0){
+		     				System.out.println("possible bas");
+		     				int res = sauvx + 1;
+		     				tab[res]=tab[sauvx];
+		     				tab[sauvx]=0;
+		     				sauvx=res;
+		     				compteur++;
+	     				}
+
+	     				
 	     			
 	     		}
 	     	}
-     	}
+			
+			if(nb==2){
+				System.out.println("gauche");
 
-     	if(nb==1){
-     		
-     		if(tab[sauvx+1]== 3){
-     			tab[sauvx+1]=tab[sauvx];
-     			System.out.println("Finis !");
-     			res=1;
-     		}else {
-	     		if((sauvx%taille) != 4 && tab[sauvx+1]!=1 ){
-	     			tab[sauvx+1]=2;
-	     			tab[sauvx]=0;
-	     			e.repaint();
-	     			/*System.out.println("bas");*/
-	     			
+	     			if(sauvx > taille-1 ){
+	     				System.out.println(sauvx);
+	     				System.out.println("test");
+	     				if(tab[sauvx-taille]==3){
+		     				System.out.println("FInis");
+		     				tab[sauvx]=0;
+		     				fin=8;
+		     			}
+	     				if(tab[sauvx-taille]==0 ){
+		     				System.out.println("possible gauche");
+		     				int res=sauvx-taille;
+		     				tab[res]=tab[sauvx];
+		     				tab[sauvx]=0;
+		     				sauvx=res;
+		     				compteur++;
+	     				}
+	     				     			
+	     				
+	     			}
+	     		
+			}
+			if(nb==3){
+				System.out.println("droite");
+	     			if(sauvx<(taille*(taille-1))){
+	     				if( tab[sauvx+taille]==3){
+		     				System.out.println("FInis");
+		     				tab[sauvx]=0;
+		     				fin=8;
+		     			}
+	     				if(tab[sauvx+taille]==0 ){
+		     				System.out.println("possible droite");
+		     				int res=sauvx+taille;
+		     				tab[res]=tab[sauvx];
+		     				tab[sauvx]=0;
+		     				sauvx=res;
+		     				compteur++;
+	     				}
+	     				
+	     			}
 	     		}
-	     	}
-     	}
+			
 
-     	if(nb==2){
-
-     		if(tab[sauvx+taille]== 3){
-     			tab[sauvx+taille]=2;
-     			System.out.println("Finis !");
-     			res=1;
-     		}else {
-	     		if(sauvx>taille-1 && tab[sauvx+taille]!=1 ){
-	     			tab[sauvx+taille]=2;
-	     			tab[sauvx]=0;
-	     			e.repaint();
-	     			/*System.out.println("droite");*/
-	     			
-	     		}
-	     	}
-     	}
-
-     	if(nb==3){
-
-     		if(tab[sauvx-taille]== 3){
-     			tab[sauvx-taille]=2;
-     			System.out.println("Finis !");
-     			res=1;
-     		}else {
-	     		if(sauvx<(taille*(taille-1)) && tab[sauvx-taille]!=1 ){
-	     			tab[sauvx-taille]=2;
-	     			tab[sauvx]=0;
-	     			e.repaint();
-	     			/*System.out.println("gauche");*/
-	     			
-	     		}
-	     	}
-     	} 	   	   	   	
-    	}
-    }
+	    }
+	    compteur=compteur+1;
+	    System.out.println("Vous avez finis en "+compteur+" coups");
+	   
+	}
 }
--- Main/CopieGrille.java
+++ Main/CopieGrille.java
@@ -7,15 +7,19 @@ import java.util.*;
     public class CopieGrille extends JFrame{
         private int[] tab;
         private int taille;
-        private int x,y;
+        private int sauvx,sauvy;
         private int res;
 
         public CopieGrille(int[] tab, int sauvx,int sauvy,int taille){
             this.taille=taille;
+            this.sauvx=sauvx;
+            this.sauvy=sauvy;
             this.setSize((taille*40)*2+30,(taille*40)+32);
             this.setLocation(100, 100);
             this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+            /*System.out.println(sauvx);
+            System.out.println(sauvy);*/
+
             GrilleCrea crea = new GrilleCrea(tab,taille);
             AlgoRand alg = new AlgoRand(crea,tab,sauvx,sauvy,taille);
             this.add(crea);
